2b32b812b77da4b63f4674347754271f86ee38f9
Deepak Kumar <mahtodeepak857@gmail.com>
2024-02-26 05:17:07 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Fragment } from "react";
+
+function App() {
+  return (
+    <Fragment>
+      <h1>Browser extension</h1>
+    </Fragment>
+  );
+}
+
+export default App;
